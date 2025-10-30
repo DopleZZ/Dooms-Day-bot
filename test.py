83c21300a231257bc2433c71979e6329499b2c70
DopleZZ <tema.bond@outlook.com>
2025-10-31 00:21:49 +0300

    context and system promt update

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 if __name__ == "__main__":
     try:
         from GigaChatSDK import rag_answer
-        ans, ctx = rag_answer("у кого биг бадонки и от кого может вонять?")
-        print("Ответ:\n", ans)
+        prmt = "у кого биг бадонки и от кого может вонять?"
+        ans, ctx = rag_answer(prmt)
+        print("входные данные: ", prmt, "\n", "Ответ:\n", ans)
         print("\nТоп-контекст:")
         for t, s in ctx:
             print(f"- score={s:.3f}: {t[:120]}...")
